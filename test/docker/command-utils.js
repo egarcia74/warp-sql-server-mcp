e0001552bb603b377799e442a45cc2e26013a0df
Eduardo Garcia-Prieto <egarcia74@gmail.com>
2026-05-18 21:56:05 +1000

    Fix Codacy findings and enhance Docker test scripts (#661)
    * fix(tests): address Codacy findings and test clarity follow-ups
    
    * feat(tests): add Codacy instructions and improve Docker test scripts

--- test/docker/command-utils.js
+++ test/docker/command-utils.js
@@ -4,27 +4,39 @@ import path from 'node:path';
 const NODE_BIN = path.dirname(process.execPath);
 
 // Safe path for execSync to satisfy Sonar S4036
-export const SAFE_PATH = `${NODE_BIN}:/usr/bin:/usr/local/bin:/usr/sbin:/usr/local/sbin:/bin:/sbin`;
+export const SAFE_PATH = [
+  NODE_BIN,
+  '/usr/bin',
+  '/usr/local/bin',
+  '/usr/sbin',
+  '/usr/local/sbin',
+  '/bin',
+  '/sbin'
+].join(path.delimiter);
 
 export function parseCommandArguments(command) {
   const args = [];
   let current = '';
   let inQuotes = false;
+  let hasToken = false;
 
   for (const char of command) {
     if (char === '"') {
       inQuotes = !inQuotes;
+      hasToken = true;
     } else if (char === ' ' && !inQuotes) {
-      if (current) {
+      if (hasToken) {
         args.push(current);
         current = '';
+        hasToken = false;
       }
     } else {
       current += char;
+      hasToken = true;
     }
   }
 
-  if (current) {
+  if (hasToken) {
     args.push(current);
   }
 
